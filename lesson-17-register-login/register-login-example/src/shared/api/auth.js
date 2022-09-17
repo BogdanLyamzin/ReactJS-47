46a634c8a4699d48e7224bab15a86cc0fe9d6026
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-09-17 15:44:25 +0300

    finish lesson 17

---
+++ lesson-17-register-login/register-login-example/src/shared/api/auth.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://auth-backend-lesson.herokuapp.com/api"
+});
+
+const setToken = (token) => {
+    if(token) {
+        return instance.defaults.headers.common.authorization = `Bearer ${token}`;
+    }
+    instance.defaults.headers.common.authorization = "";
+}
+
+export const signup = async (data) => {
+    const {data: result} = await instance.post("/users/signup", data);
+    setToken(result.token);
+    return result;
+}
+
+export const login = async (data) => {
+    const {data: result} = await instance.post("/users/login", data);
+    setToken(result.token);
+    return result;
+}
+
+export const logout = async() => {
+    const data = await instance.post("/users/logout");
+    setToken();
+    return data;
+}
+
+export const getCurrent = async(token) => {
+    try {
+        setToken(token);
+        const {data} = await instance.get("/users/current");
+        return data;
+    } catch (error) {
+        setToken();
+        throw error;
+    }
+}
+
+export default instance;
